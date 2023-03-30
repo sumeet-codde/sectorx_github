e5d02527e676da6e2000711a2f496d35d6094a1c
Sumeet Ghodke <66643575+sumeet-codde@users.noreply.github.com>
2023-03-30 11:04:14 +0530

    folders added

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Homepage, News, Cryptocurrencies, CryptoDetails, Navbar, Wallet } from './components';
+import './App.css';
+// import './index.css';
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetails />
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+            <Route exact path="/wallet">
+              <Wallet />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2023
+          <Link to="/">
+            SectorX
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+          <Link to="/wallet">Wallet</Link>
+        </Space>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
+
